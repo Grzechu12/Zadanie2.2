2fb3fb405169350f2108abb5bf63e07b9bca9193
Grzechu12 <161622135+Grzechu12@users.noreply.github.com>
2024-03-13 16:06:11 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    for(int=1; i<=100; i++){
+       System.out.println(i);
+    };
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
